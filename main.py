b452585818a286debbc73c5cd3fedfeaf7faedab
PixxelCurs <taysen.doering2@gmail.com>
2025-08-31 13:25:09 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.py
@@ -0,0 +1,116 @@
+def wait_sek(sekunden: number):
+    control.wait_micros(1000000 * sekunden)
+print(":)")
+wait_sek(1)
+print(":(")
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+word_list = ["kaka", "alex", "sandra", "banane", "apfel"]
+
+
+word = word_list[randint(0, len(word_list) - 1)]
+
+# creates an variable with an empty value
+guesses = ''
+
+# determine the number of turns
+turns = 10
+buttonklick = 0
+auswahl = 0
+
+
+    
+# check if the turns are more than zero
+while turns > 0:
+    buttonklick = 0
+
+    # make a counter that starts with zero
+    failed = 0
+# Create a_word
+    for char in word:
+
+        # see if the character is in the players guess
+        if char in guesses:
+
+            # print then out the character
+            print(char ),
+
+        else:
+
+            # if not found, print a dash
+            print("_"),
+
+            # and increase the failed counter with one
+            failed = +1
+
+            # if failed is equal to zero
+
+    # print You Won
+    if failed == 0:
+        print("You won")
+        # exit the script
+        break
+        # ask the user go guess a character
+    print("auswahl:")
+    print(alphabet[auswahl])   
+    while buttonklick == 0:
+        if input.button_is_pressed(Button.A):
+            auswahl = auswahl +1
+            print("auswahl:")
+            print(alphabet[auswahl])
+            wait_sek(1)
+        elif input.button_is_pressed(Button.B):
+            guess = (alphabet[auswahl])
+            print("weiter gehts")
+            buttonklick = 1
+            wait_sek(1)
+        
+
+
+    # set the players guess to guesses
+    guesses = guesses + guess
+
+    # if the guess is not found in the secret word
+    if guess not in word:
+
+        # turns counter decreases with 1 (now 9)
+        turns = -1
+
+        # print wrong
+        print("Wrong")
+        basic.show_icon(IconNames.SAD)
+
+
+        # how many turns are left
+      
+        
+        print("You have")
+        print(turns)
+        print("more guesses")
+    
+        # if the turns are equal to zero
+        if turns == 0:
+            # print "You Lose"
+            print("You Lose")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,87 @@
+let failed: number;
+let guess: string;
+function wait_sek(sekunden: number) {
+    control.waitMicros(1000000 * sekunden)
+}
+
+console.log(":)")
+wait_sek(1)
+console.log(":(")
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let word_list = ["kaka", "alex", "sandra", "banane", "apfel"]
+let word = word_list[randint(0, word_list.length - 1)]
+//  creates an variable with an empty value
+let guesses = ""
+//  determine the number of turns
+let turns = 10
+let buttonklick = 0
+let auswahl = 0
+//  check if the turns are more than zero
+while (turns > 0) {
+    buttonklick = 0
+    //  make a counter that starts with zero
+    failed = 0
+    //  Create a_word
+    for (let char of word) {
+        //  see if the character is in the players guess
+        if (guesses.indexOf(char) >= 0) {
+            //  print then out the character
+            console.log(char)
+        } else {
+            //  if not found, print a dash
+            console.log("_")
+            //  and increase the failed counter with one
+            failed = +1
+        }
+        
+    }
+    //  if failed is equal to zero
+    //  print You Won
+    if (failed == 0) {
+        console.log("You won")
+        //  exit the script
+        break
+    }
+    
+    //  ask the user go guess a character
+    console.log("auswahl:")
+    console.log(alphabet[auswahl])
+    while (buttonklick == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            auswahl = auswahl + 1
+            console.log("auswahl:")
+            console.log(alphabet[auswahl])
+            wait_sek(1)
+        } else if (input.buttonIsPressed(Button.B)) {
+            guess = alphabet[auswahl]
+            console.log("weiter gehts")
+            buttonklick = 1
+            wait_sek(1)
+        }
+        
+    }
+    //  set the players guess to guesses
+    guesses = guesses + guess
+    //  if the guess is not found in the secret word
+    if (word.indexOf(guess) < 0) {
+        //  turns counter decreases with 1 (now 9)
+        turns = -1
+        //  print wrong
+        console.log("Wrong")
+        basic.showIcon(IconNames.Sad)
+        //  how many turns are left
+        console.log("You have")
+        console.log(turns)
+        console.log("more guesses")
+        //  if the turns are equal to zero
+        if (turns == 0) {
+            //  print "You Lose"
+            console.log("You Lose")
+        }
+        
+    }
+    
+}
+basic.forever(function on_forever() {
+    
+})
